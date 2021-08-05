794ef3a4d661faf2d91ea4ddd3cbdc773fce7922
Pedro H Santos B <pedrosaints@alu.ufc.br>
2021-08-05 02:11:51 -0300

    add q2

---
+++ Trabalho1/q2.asm
@@ -0,0 +1,25 @@
+goto main
+     wb 0
+
+res  ww 12
+A    ww 1
+B    ww 0
+
+hlt  halt
+main += s, res
+     if_zero s, hlt
+     -- s
+     if_zero s, hlt
+     -- s
+     ++ x
+     if_zero s, end
+loop mov x, B
+     += x, A
+     -- s
+     mov x, res
+     if_zero s, hlt
+     zera y
+     += y, B
+     mov y, A
+     goto loop
+end mov x, res
